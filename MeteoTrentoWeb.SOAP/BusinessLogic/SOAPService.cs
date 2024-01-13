06f2e4b57db44cd7fef04151269115e230d15d66
Simone <simonemorello48@gmail.com>
2024-01-13 11:18:08 +0100

    Implementazione SOAP Service

---
+++ MeteoTrentoWeb.SOAP/BusinessLogic/SOAPService.cs
@@ -0,0 +1,37 @@
+﻿using System.ServiceModel;
+using MeteoTrentoWeb.Modelli;
+using Newtonsoft.Json;
+
+
+namespace MeteoTrentoWeb.SOAP.BusinessLogic;
+
+[ServiceContract]
+public interface ISOAPService
+{
+    [OperationContract]
+    Giorni OttieniPrevisione(string data);
+}
+
+public class SoapService : ISOAPService
+{
+    public Giorni OttieniPrevisione(string data)
+    {
+        string uri = "https://www.meteotrentino.it/protcivtn-meteo/api/front/previsioneOpenDataLocalita?localita=TRENTO";
+        using (HttpClient client = new HttpClient())
+        {
+            using (HttpResponseMessage response = client.GetAsync(uri).Result)
+            {
+                using (HttpContent content = response.Content)
+                {
+                    string result = content.ReadAsStringAsync().Result;
+                    RootObject modello = JsonConvert.DeserializeObject<RootObject>(result);
+                    foreach (var previsione in modello.previsione)
+                    {
+                        return previsione.giorni.Where(giorni => giorni.giorno.Equals(data)).First();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
--- MeteoTrentoWeb.SOAP/Program.cs
+++ MeteoTrentoWeb.SOAP/Program.cs
@@ -1,6 +1,18 @@
+using SoapCore;
+using MeteoTrentoWeb.SOAP.BusinessLogic;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSoapCore();
+builder.Services.AddScoped<ISOAPService, SoapService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.UseSoapEndpoint<ISOAPService>("/Service2.asmx", new SoapEncoderOptions(), SoapSerializer.XmlSerializer);
+});
 
 app.Run();
